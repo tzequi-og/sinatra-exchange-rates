3e4422a9cae8452e269c535ad12bef73aeed579c
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 04:22:22 +0000

    final

--- app.rb
+++ app.rb
@@ -29,3 +29,17 @@ get("/:currency") do
 
   erb(:currency)
 end
+
+get("/:currency_from/:currency_to") do
+  @currency_from = params.fetch(:currency_from).to_s
+  @currency_to = params.fetch(:currency_to).to_s
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@currency_from}&to=#{@currency_to}&amount=1"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+
+  @exchange_rate = parsed_data.fetch("result")
+
+  erb(:flexible)
+end
