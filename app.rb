7ade04e2be0f57b20a1b24a568d27a6e551d315c
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 04:12:54 +0000

    second_list

--- app.rb
+++ app.rb
@@ -15,3 +15,17 @@ get("/") do
 
   erb(:homepage)
 end
+
+get("/:currency") do
+  @currency_from = params.fetch(:currency).to_s
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+
+  @currencies_full = parsed_data.fetch("currencies")
+  @currencies = @currencies_full.keys
+
+  erb(:currency)
+end
